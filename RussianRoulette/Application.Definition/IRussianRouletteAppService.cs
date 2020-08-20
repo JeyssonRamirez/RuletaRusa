c01a879072322882f2e4f7b27f3fb85fb2d6973a
Jeysson Ramirez <jeysson.ramirez@outlook.com>
2020-08-20 15:01:29 -0500

    using AppServices

--- RussianRoulette/Application.Definition/IRussianRouletteAppService.cs
+++ RussianRoulette/Application.Definition/IRussianRouletteAppService.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using Core.DataTransferObject;
+using Core.Entities;
 
 namespace Application.Definition
 {
     public interface IRussianRouletteAppService
     {
         BaseApiResult GetAll();
+        BaseApiResult CreateRoulette(Roulette roulette);
     }
 }
